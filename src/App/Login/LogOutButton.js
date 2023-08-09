49f53335ebd41dd4185af313162ed8e5ca95c7e1
jdabassett <jacobbassett@gmail.com>
2023-08-09 12:51:13 -0600

    authentication working

---
+++ src/App/Login/LogOutButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from 'react-bootstrap/Button';
+
+export default function LogOutButton(){
+  const { logout } = useAuth0();
+
+  return (
+    <Button
+      className="logButtons"
+      onClick={() => logout({ logoutParams: { returnTo: process.env.REACT_APP_AUTH_REDIRECT_URI } })}>
+      Log-Out
+    </Button>
+  );
+};
+
--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,10 +1,16 @@
 
 import React from 'react';
+import {useAuth0} from '@auth0/auth0-react';
+import LogOutButton from '../Login/LogOutButton.js';
 
 export default function Main () {
+  let {isAuthenticated,user}=useAuth0();
+  // console.log(isAuthenticated);
   return (
     <div className="main-container">
+      {isAuthenticated?<p>Hello {user.name}</p>:null}
       <p>Main Boogers</p>
+      <LogOutButton/>
     </div>
   )
 }
