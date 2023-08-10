b93a5dec952d95087956a0ac3c8d1e155be30a05
jdabassett <jacobbassett@gmail.com>
2023-08-09 22:06:12 -0600

    small changes working on search page

--- src/App/Main/Search/Search.js
+++ src/App/Main/Search/Search.js
@@ -1,10 +1,57 @@
 
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export default function Search () {
+  let [searchState,setSearchState]=React.useState({
+    searchType:null,
+  })
+  
+  console.log(searchState.searchType)
   return (
     <div className="search-container">
-      <p>Search Boogers</p>
+      <Form>
+        <Form.Check 
+          type="radio"
+          id="default-radio"
+          label="Random"
+          name="searchRandomBool"
+          onClick={(prevState)=>setSearchState({...prevState,searchType:'random'})}
+          inline
+        />
+        <Form.Check 
+          type="radio"
+          id="default-radio"
+          label="Name"
+          name="searchRandomBool"
+          onClick={(prevState)=>setSearchState({...prevState,searchType:'name'})}
+          inline
+        />
+        <Form.Check 
+          type="radio"
+          id="default-radio"
+          label="Alcohol"
+          name="searchRandomBool"
+          onClick={(prevState)=>setSearchState({...prevState,searchType:'alcohol'})}
+          inline
+        />
+        <Form.Check 
+          type="radio"
+          id="default-radio"
+          label="Category"
+          name="searchRandomBool"
+          onClick={(prevState)=>setSearchState({...prevState,searchType:'category'})}
+          inline
+        />
+        <Form.Check 
+          type="radio"
+          id="default-radio"
+          label="Glass"
+          name="searchRandomBool"
+          onClick={(prevState)=>setSearchState({...prevState,searchType:'glass'})}
+          inline
+        />
+      </Form>
     </div>
   )
 }
